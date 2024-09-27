63723b7eb8438fb4d7c36964d58ca16e3252a356
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-09-27 16:54:07 +0800

    Refactor score status program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,16 +7,24 @@ MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
 PASSABLE_THRESHOLD = 50
-message = ""
 
-score = float(input("Enter score: "))
-if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-    message = "Invalid score"
-elif score >= EXCELLENT_THRESHOLD:
-    message = "Excellent"
-elif score >= PASSABLE_THRESHOLD:
-    message = "Passable"
-else:
-    message = "Bad"
 
-print(message)
+def main():
+    """Score status program"""
+    score = float(input("Enter score: "))
+    message = determine_score_status(score)
+    print(message)
+
+
+def determine_score_status(score: float) -> str:
+    """Determine status from score"""
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        return "Invalid score"
+    elif score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASSABLE_THRESHOLD:
+        return "Passable"
+    return "Bad"
+
+
+main()
