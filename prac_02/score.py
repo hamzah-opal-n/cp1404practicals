e7e57018bf00ba1ab25390c7fabfa7a47266d82b
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-09-27 17:07:14 +0800

    Add random score generator to score status program

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,6 +3,8 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+from random import randint
+
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
@@ -10,10 +12,12 @@ PASSABLE_THRESHOLD = 50
 
 
 def main():
-    """Score status program"""
+    """Score status program with random score generator"""
     score = float(input("Enter score: "))
     message = determine_score_status(score)
     print(message)
+    random_score = randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+    print(f"Random score: {random_score}\nStatus: {determine_score_status(random_score)}")
 
 
 def determine_score_status(score: float) -> str:
