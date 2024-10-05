59fefc7693a886240fb877c5de5117e957ef183c
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-05 15:59:12 +0800

    Add files program

---
+++ prac_03/files.py
@@ -0,0 +1,27 @@
+"""Files Exercise"""
+
+
+def main():
+    question_1()
+    question_2()
+    question_3()
+    question_4()
+
+
+def question_1():
+    pass
+
+
+def question_2():
+    pass
+
+
+def question_3():
+    pass
+
+
+def question_4():
+    pass
+
+
+main()
