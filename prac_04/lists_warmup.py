a8dfb0f54b6cd32fe4e4c50f4f8047e1bd4e2d45
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-07 16:57:46 +0800

    Add code statements

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,3 +11,15 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # False
 # False
 # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# 1.
+numbers[0] = "ten"
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
