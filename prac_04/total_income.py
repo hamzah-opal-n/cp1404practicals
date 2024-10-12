535d2c0dd92b1cd352082ad41dc48de613ec0bf5
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-13 01:22:09 +0800

    Refactor print report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,17 +8,22 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
+    print()
 
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report(incomes)
+
+
+def print_report(incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for i in range(len(incomes)):
+        income = incomes[i]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {i + 1:2} - Income: ${income:10.2f}         Total: ${total:10.2f}")
 
 
 main()
