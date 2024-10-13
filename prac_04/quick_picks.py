8f2156ac7a2e94c1ace7c90564db248ada6457e9
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-13 13:47:09 +0800

    Implement quick pick random generator

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,1 +1,14 @@
 """Quick Pick Lottery Ticket Generator"""
+
+import random
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
+pick = []
+for i in range(NUMBERS_PER_PICK):
+    number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+    while number in pick:
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+    pick.append(number)
+print(sorted(pick))
