33cf348a169a668e7bdcce01f1e748d6e35e961d
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-16 21:42:47 +0800

    Add hex colours program with colour name to hex code dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+"""Hex Colours"""
+
+COLOUR_NAME_TO_HEX_CODE = {"aliceblue": "#f0f8ff",
+                           "baby pink": "#f4c2c2",
+                           "banana mania": "asdf",
+                           "dark byzantium": "#5d3954",
+                           "heliotrope": "#df73ff",
+                           "pictorial carmine": "#c30b4e",
+                           "rebeccapurple": "#663399",
+                           "school bus yellow": "#ffd800",
+                           "tiffany blue": "#0abab5",
+                           "xanadu": "#738678"}
