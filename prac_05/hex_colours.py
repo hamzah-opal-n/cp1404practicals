195f17718817be76f0a1021b56b7d00462312106
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-16 21:46:08 +0800

    Complete hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,3 +10,12 @@ COLOUR_NAME_TO_HEX_CODE = {"aliceblue": "#f0f8ff",
                            "school bus yellow": "#ffd800",
                            "tiffany blue": "#0abab5",
                            "xanadu": "#738678"}
+
+colour_name = input("Enter colour name: ").lower()
+while colour_name != "":
+    try:
+        hex_code = COLOUR_NAME_TO_HEX_CODE[colour_name]
+        print(f"Hex code: {hex_code}")
+    except KeyError:
+        print(f"Invalid colour name!")
+    colour_name = input("Enter colour name: ").lower()
