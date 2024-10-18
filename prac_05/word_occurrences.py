84694814dade102cea932a16f9b264a3c62e24de
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-18 20:44:26 +0800

    Implement word count loop

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,13 @@ Word Occurrences
 Estimate: 20 minutes
 Actual:   ?? minutes
 """
+
+words = input("Text: ").split()
+word_to_count = {}
+for word in words:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+
+print(word_to_count)
