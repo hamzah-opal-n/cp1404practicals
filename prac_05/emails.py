67179633a2a1416c5e5bfa3a2702705ca5c47a29
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-18 22:54:47 +0800

    Implement basic program flow

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,20 @@ Emails
 Estimate: 20 minutes
 Actual:   ?? minutes
 """
+
+
+def main():
+    """Create a database of emails and names based on user input"""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = email
+        choice = input(f"Is your name {name}? (Y/n)").upper()
+        if choice not in ("Y", ""):
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    print(email_to_name)
+
+
+main()
