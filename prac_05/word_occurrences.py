9540f82d5fcedec8c434d931830407c6ee3b330e
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-18 20:55:03 +0800

    Implement string formatting for output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:   ?? minutes
+Actual:   13 minutes
 """
 
 words = input("Text: ").split()
@@ -12,5 +12,6 @@ for word in words:
     except KeyError:
         word_to_count[word] = 1
 
+max_word_length = max(len(word) for word in word_to_count.keys())
 for word, count in sorted(word_to_count.items()):
-    print(f"{word} : {count}")
+    print(f"{word:{max_word_length}} : {count}")
