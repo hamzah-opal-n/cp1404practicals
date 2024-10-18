f6153ae4a6b33da50bc700259ad936b09898a084
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-18 23:00:55 +0800

    Implement name extraction and output formatting

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,7 +1,7 @@
 """
 Emails
 Estimate: 20 minutes
-Actual:   ?? minutes
+Actual:   12 minutes
 """
 
 
@@ -10,13 +10,20 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        name = email
-        choice = input(f"Is your name {name}? (Y/n)").upper()
+        name = extract_name(email)
+        choice = input(f"Is your name {name}? (Y/n) ").upper()
         if choice not in ("Y", ""):
             name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
-    print(email_to_name)
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    """Extract name from email"""
+    name = " ".join(email.split("@")[0].split(".")).title()
+    return name
 
 
 main()
