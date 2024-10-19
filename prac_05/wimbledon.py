a4ae22850b44c7bd31818734376ab378a2461239
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-19 15:26:32 +0800

    Implement file reading and record processing

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,3 +3,37 @@ Wimbledon
 Estimate: 30 minutes
 Actual:   ?? minutes
 """
+
+FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
+
+
+def main():
+    """Program to display the Wimbledon champions and their countries"""
+    records = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            records.append(line.strip().split(","))
+    champion_to_count, winning_countries = process_records(records)
+    print(champion_to_count)
+    print(winning_countries)
+
+
+def process_records(records):
+    """Extract champion win counts and countries from records"""
+    champion_to_count = {}
+    winning_countries = set()
+    for record in records:
+        country = record[COUNTRY_INDEX]
+        champion = record[CHAMPION_INDEX]
+        winning_countries.add(country)
+        try:
+            champion_to_count[champion] += 1
+        except KeyError:
+            champion_to_count[champion] = 1
+    return champion_to_count, winning_countries
+
+
+main()
