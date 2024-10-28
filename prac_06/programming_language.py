76ea60b61eac7d9973eb586c844df191c038b64a
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-10-28 22:41:57 +0800

    Implement ProgrammingLanguage class with is_dynamic method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,3 +6,23 @@ End Time:   22xx
 Time Taken: ?? minutes
 """
 
+
+class ProgrammingLanguage:
+    """Represent a Programming Language object."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a Programming Language instance.
+
+        name: string
+        typing: string
+        reflection: boolean
+        year: int
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Determine if a Programming Language is dynamic."""
+        return self.typing == "Dynamic"
