af01a8dcb92c4405d18f5a27af3f379207455bfb
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-01 23:12:57 +0800

    Implement Guitar class and required methods

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,37 @@
 """
 Guitar
 Estimate:   30 minutes
-Start Time: xxxx
+Start Time: 2305
 End Time:   xxxx
 Time Taken: xx minutes
 """
+
+CURRENT_YEAR = 2024
+VINTAGE_AGE_THRESHOLD = 50
+
+
+class Guitar:
+    """Represent a Guitar object."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Car instance.
+
+        name: string
+        year = integer
+        cost: float
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """String to represent a Guitar instance."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Calculate the age of the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if a guitar is vintage."""
+        return self.get_age() >= VINTAGE_AGE_THRESHOLD
