dadd2725d0332b63e8846f7fe666f2507e09afbe
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-09 21:03:13 +0800

    Implement save_guitars function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -6,8 +6,9 @@ FILENAME = "guitars.csv"
 
 
 def main():
-    """Program to view guitar data"""
+    """Program to view and add guitar data"""
     guitars = load_guitars(FILENAME)
+    guitars.sort()
     display_guitars(guitars)
 
     print("\nAdd new guitars (leave name blank to finish)")
@@ -20,11 +21,14 @@ def main():
         print(f"{guitar} added.\n")
         name = input("Name: ")
 
+    guitars.sort()
     display_guitars(guitars)
+    save_guitars(guitars, FILENAME)
+    print(f"Guitars saved to {FILENAME}")
 
 
 def load_guitars(filename):
-    """Load guitars from filename"""
+    """Load guitars from csv file"""
     guitars = []
     with open(filename, "r") as in_file:
         for line in in_file:
@@ -34,11 +38,23 @@ def load_guitars(filename):
     return guitars
 
 
+def save_guitars(guitars, filename):
+    """Save guitars to csv file"""
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            fields = (guitar.name, str(guitar.year), str(guitar.cost))
+            out_file.write(",".join(fields) + "\n")
+
+
 def display_guitars(guitars):
     """Display all guitars sorted by year"""
-    print("My guitars:")
-    for guitar in sorted(guitars):
-        print(guitar)
+    name_length = max(len(guitar.name) for guitar in guitars)
+    cost_length = max(len(f"{guitar.cost:,.2f}") for guitar in guitars)
+    print("\nMy guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>{name_length}} ({guitar.year}), "
+              f"worth $ {guitar.cost:{cost_length},.2f}{vintage_string}")
 
 
 main()
