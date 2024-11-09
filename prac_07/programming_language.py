a25e90b28e92c0db7848ccc05ac70dc0dd97b2a6
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-09 20:06:42 +0800

    Complete programming language modifications

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,25 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
-        """Construct a ProgrammingLanguage from the given values."""
+    def __init__(self, name: str, typing: str, reflection: bool, year: int, pointer_arithmetic: bool):
+        """Construct a ProgrammingLanguage from the given values.
+
+        name: string
+        typing: string
+        reflection: boolean
+        year: int
+        pointer_arithmetic: boolean
+        """
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -25,9 +34,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
 
     languages = [ruby, python, visual_basic]
     print(python)
