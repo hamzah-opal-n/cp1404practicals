81b6017009d340b71b0ab49417bebd2c3375484b
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-10 13:02:03 +0800

    Create Project class

---
+++ prac_07/project.py
@@ -0,0 +1,29 @@
+PROJECT_COMPLETE_PERCENTAGE = 100
+
+
+class Project:
+    """Represent a Project object."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, percent_complete):
+        """Initialise a Project instance.
+
+        name: string
+        start_date: string
+        priority = integer
+        cost_estimate: float
+        percent_complete: integer
+        """
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.percent_complete = percent_complete
+
+    def __str__(self):
+        """String to represent a Project instance."""
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate: ${self.cost_estimate:.2f}, completion: {self.percent_complete}%")
+
+    def is_complete(self):
+        """Determine if a Project is complete."""
+        return self.percent_complete >= PROJECT_COMPLETE_PERCENTAGE
