3932e026ba6852176eabd7cb44f10fc5e232be2b
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-10 12:50:08 +0800

    Implement main menu

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,7 +1,47 @@
 """
 Project Management Program
 Estimate:   90 minutes
-Start Time: xxxx
+Start Time: 1242
 End Time:   xxxx
-Time Taken: xx minutes
+Time Taken: xx minutes (with breaks)
 """
+
+DEFAULT_FILENAME = "projects.txt"
+MENU = ("- (L)oad projects\n"
+        "- (S)ave projects)\n"
+        "- (D)isplay projects\n"
+        "- (F)ilter projects by date\n"
+        "- (A)dd new project\n"
+        "- (U)pdate project\n"
+        "- (Q)uit")
+INPUT_PROMPT = ">>> "
+
+
+def main():
+    """Project management program"""
+    print("Welcome to Pythonic Project Management")
+    # Load projects from DEFAULT_FILENAME
+    print(MENU)
+    menu_choice = input(INPUT_PROMPT).upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            print("load from different file")
+        elif menu_choice == "S":
+            print("save to filename")
+        elif menu_choice == "D":
+            print("display projects")
+        elif menu_choice == "F":
+            print("filter projects")
+        elif menu_choice == "A":
+            print("add new project")
+        elif menu_choice == "U":
+            print("update project")
+        else:
+            print("Invalid choice")
+        print(MENU)
+        menu_choice = input(INPUT_PROMPT).upper()
+    # ask if they want to save to default file
+    print("Thank you for using custom-built project management software.")
+
+
+main()
