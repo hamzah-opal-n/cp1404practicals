a0f8a710b5a379140764f5f2ac9576f1e47cb731
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-17 11:42:36 +0800

    Implement modification steps 8 and 9

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,7 +9,7 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        self.root.ids.output_label.text = "Hello "
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 BoxLayoutDemo().run()
