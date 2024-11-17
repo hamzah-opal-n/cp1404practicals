08b65b7da81e4fc884d88374bd344cc0fd2f6ccf
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-17 13:36:19 +0800

    Implement input handling

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -21,12 +21,23 @@ class MilesToKilometresConverter(App):
 
     def handle_conversion(self):
         """Handle conversion from miles to kilometres"""
-        self.distance_in_kilometres = str(float(self.root.ids.input_number.text) * MILES_TO_KILOMETRES_RATE)
+        miles = self.get_valid_miles()
+        kilometres = miles * MILES_TO_KILOMETRES_RATE
+        self.distance_in_kilometres = str(kilometres)
+
+    def get_valid_miles(self):
+        """Ensure that the value in the miles text input is valid."""
+        try:
+            miles = float(self.root.ids.input_number.text)
+            return miles
+        except ValueError:
+            return 0.0
 
     def handle_increment(self, increment):
         """Handle increment of specified value"""
-        value = float(self.root.ids.input_number.text) + increment
-        self.root.ids.input_number.text = str(value)
+        miles = self.get_valid_miles()
+        miles += increment
+        self.root.ids.input_number.text = str(miles)
 
 
 MilesToKilometresConverter().run()
