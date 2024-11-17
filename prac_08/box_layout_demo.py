363e16e0e6eba343f2c60667e209f881d3faebfb
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-17 11:44:55 +0800

    Implement clear button event handler (modification step 10)

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -5,11 +5,15 @@ from kivy.lang import Builder
 class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
-        self.root = Builder.load_file('box_layout.kv')
+        self.root = Builder.load_file("box_layout.kv")
         return self.root
 
     def handle_greet(self):
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
