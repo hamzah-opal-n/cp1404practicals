a7fa7553e86c84c1e6f06bf4f724f9b6ad7c183c
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 21:03:36 +0800

    Change reliability to an actual float

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,7 +2,7 @@
 
 from prac_09.unreliable_car import UnreliableCar
 
-my_broken_car = UnreliableCar("Death Trap", 1000, 50)
+my_broken_car = UnreliableCar("Death Trap", 1000, 50.0)
 for i in range(10):
     my_broken_car.drive(60)
     print(my_broken_car)
