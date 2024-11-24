28c4e10acd8239be05eac8aefb1fefed35c0da6f
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 22:40:57 +0800

    Complete Band class definition

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,6 +1,34 @@
 """Band class for CP1404"""
 
+
 class Band:
     """Band class"""
 
-    def __init__(self):
+    def __init__(self, name=""):
+        """Construct a Band with a name and empty list of members."""
+        self.name = name
+        self.members = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        member_strings = []
+        for member in self.members:
+            member_strings.append(str(member))
+        return f"{self.name} ({', '.join(member_strings)})"
+
+    def __repr__(self):
+        """Return a string representation of a Band, showing the variables."""
+        return str(vars(self))
+
+    def add(self, member):
+        """Add a member to the band."""
+        self.members.append(member)
+
+    def play(self):
+        """Return a string showing the band members playing their first (or no) instrument."""
+        if not self.members:
+            return f"{self.name} has no band members!"
+        play_strings = []
+        for member in self.members:
+            play_strings.append(member.play())
+        return "\n".join(play_strings)
