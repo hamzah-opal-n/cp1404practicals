7fae30204a1a3f1e2fb811aca488563c56a9e8f6
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 23:19:46 +0800

    Implement choose taxi code

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -16,17 +16,33 @@ def main():
     print("Let's drive!")
     print(MENU)
     menu_choice = input(">>> ").lower()
+
     while menu_choice != "q":
+
         if menu_choice == "c":
             print("Taxis available:")
             display_taxis(taxis)
+            taxi_choice = input("Choose taxi: ")
+            try:
+                taxi_choice = int(taxi_choice)
+                if taxi_choice >= 0:
+                    current_taxi = taxis[taxi_choice]
+                else:
+                    print("Invalid taxi choice")
+            except (IndexError, ValueError):
+                print("Invalid taxi choice")
+
         elif menu_choice == "d":
             print("Drive")
+            print(current_taxi)
+
         else:
             print("Invalid option")
+
         print(f"Bill to date: ${total_bill:.2f}")
         print(MENU)
         menu_choice = input(">>> ").lower()
+
     print(f"Total trip cost: ${total_bill:.2f}")
     print("Taxis are now:")
     display_taxis(taxis)
