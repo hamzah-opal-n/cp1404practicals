8f0f3bba070dd5e5feffd2362e82a2f1393c3151
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 21:27:45 +0800

    Complete SilverServiceTaxi class tests

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,1 +1,10 @@
 """SilverServiceTaxi class test"""
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+hummer = SilverServiceTaxi("Hummer", 200, 4.0)
+print(hummer)
+
+fancy_taxi = SilverServiceTaxi("Fancy Taxi", 1000, 2.0)
+fancy_taxi.drive(18)
+assert fancy_taxi.get_fare() == 48.78
