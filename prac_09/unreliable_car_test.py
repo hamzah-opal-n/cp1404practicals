6fed890e7de31b423ece74981b2a13b3d8b1ae62
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 21:02:53 +0800

    Complete UnreliableCar class test program

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,1 +1,8 @@
 """UnreliableCar class test"""
+
+from prac_09.unreliable_car import UnreliableCar
+
+my_broken_car = UnreliableCar("Death Trap", 1000, 50)
+for i in range(10):
+    my_broken_car.drive(60)
+    print(my_broken_car)
