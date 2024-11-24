570efa246743971b4817b4f676430c76f7403b5b
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-24 23:06:52 +0800

    Implement display taxis function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,7 +7,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
-    """Taxi Simulator program"""
+    """Taxi Simulator program."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
@@ -17,7 +17,8 @@ def main():
     menu_choice = input(">>> ").lower()
     while menu_choice != "q":
         if menu_choice == "c":
-            print("Choose Taxi")
+            print("Taxis available:")
+            display_taxis(taxis)
         elif menu_choice == "d":
             print("Drive")
         else:
@@ -27,6 +28,13 @@ def main():
         menu_choice = input(">>> ").lower()
     print("Total trip cost: $xxx.xx")
     print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display taxis with numbered position in list."""
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
 
 
 main()
