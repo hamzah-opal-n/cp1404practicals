9e697f214a842a04c77a27638ad5973e5606cc65
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-30 15:41:03 +0800

    Complete task 2

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -32,7 +32,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+    # 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -41,11 +41,14 @@ def run_tests():
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     car = Car(fuel=10)
+    assert car.fuel == 10, "Car does not set fuel correctly"
+    empty_car = Car()
+    assert empty_car.fuel == 0, "Car does not set fuel correctly"
 
 
 run_tests()
