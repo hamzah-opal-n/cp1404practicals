2a747fd4ee96e9bfea4ff9a97f44e88927ec1697
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-30 16:10:13 +0800

    Implement exception handling for DisambiguationError

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,6 +1,7 @@
 """Wikipedia Page Search"""
 
 import wikipedia
+from wikipedia import DisambiguationError
 
 INPUT_PROMPT = "Enter page title: "
 
@@ -9,10 +10,15 @@ def main():
     """Program to search for Wikipedia pages based on user input."""
     search_term = input(INPUT_PROMPT)
     while search_term != "":  # while input is not blank
-        page = wikipedia.page(search_term)
-        print(page.title)
-        print(page.summary)
-        print(page.url)
+        try:
+            page = wikipedia.page(search_term, auto_suggest=False)
+            print(page.title)
+            print(page.summary)
+            print(page.url)
+        except DisambiguationError:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(wikipedia.search(search_term))
+        print()
         search_term = input(INPUT_PROMPT)
     print("Thank you.")
 
