129a7bf0d0da03c95c9bdd4f203d74c1cc90e950
hamzah-opal-n <hamzah.nutt@my.jcu.edu.au>
2024-11-30 16:05:30 +0800

    Implement printing of page details

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,1 +1,20 @@
 """Wikipedia Page Search"""
+
+import wikipedia
+
+INPUT_PROMPT = "Enter page title: "
+
+
+def main():
+    """Program to search for Wikipedia pages based on user input."""
+    search_term = input(INPUT_PROMPT)
+    while search_term != "":  # while input is not blank
+        page = wikipedia.page(search_term)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+        search_term = input(INPUT_PROMPT)
+    print("Thank you.")
+
+
+main()
